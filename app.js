bb243610c92da185d72a991e5c0a370e682ed273
RijlanM10 <rijlan01@gmail.com>
2021-02-04 10:24:40 +0700

    Deleting Data

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
-    li.setAttribute('data-id', doc.data().id);
+    li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        // let id = doc.id;
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    });
 }
 
 // getting data
@@ -31,7 +41,7 @@ form.addEventListener('submit', (e) => {
         name: form.name.value,
         city: form.city.value
     });
-    
+
     form.name.value = '';
     form.city.value = '';
 });
