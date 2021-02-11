d784632424f4f3edbae650b136584cf877a4ce83
RijlanM10 <rijlan01@gmail.com>
2021-02-11 11:11:15 +0700

    Introduction, Setup, HTML Template

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,9 @@
-const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-admin.initializeApp();
+const functions = require('firebase-functions');
 
-exports.addAdminRole = functions.https.onCall((data, context) => {
-
-    if (context.auth.token.admin !== true) {
-        return { error: 'only admin can add other admin, sucker' }
-    }
-
-    // get user and add custom claim (admin)
-    return admin.auth().getUserByEmail(data.email).then(user => {
-        return admin.auth().setCustomUserClaims(user.uid, {
-            admin: true
-        });
-    }).then(() => {
-        return {
-            message: `Success! ${data.email} has been made an admin`
-        }
-    }).catch(err => {
-        return err;
-    })
-});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
