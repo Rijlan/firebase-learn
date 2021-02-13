7289f7e3314912642fe8f8232a2a2bc6f52b0d96
RijlanM10 <rijlan01@gmail.com>
2021-02-13 07:24:35 +0700

    Callable Functions

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,9 @@ exports.randomNumber = functions.https.onRequest((request, response) => {
     const number = Math.round(Math.random() * 100);
     response.send(number.toString());
 });
+
+//  http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+    const name = data.name;
+    return `Hello, ${name}`;
+});
